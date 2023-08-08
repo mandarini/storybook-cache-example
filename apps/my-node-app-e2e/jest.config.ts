e5643eda1f9bf475eeba93cd6abbadbc03746da5
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-08-08 13:34:50 +0300

    added node lib

---
+++ apps/my-node-app-e2e/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'my-node-app-e2e',
+  preset: '../..//jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../..//coverage/my-node-app-e2e',
+};
